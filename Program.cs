2274a0f14a7121fb0814aeadb19047acab3cc7ea
Dan Krusi <dan@nerves.ch>
2022-11-04 10:38:16 +0100

    intitial empty app

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsVirtualDesktopHelper {
+    class Program {
+
+        [STAThread]
+        public static void Main(string[] args) {
+            Console.WriteLine("asdf");
+            Application.EnableVisualStyles();
+            Application.Run(new Form()); // or whatever
+        }
+    }
+}
