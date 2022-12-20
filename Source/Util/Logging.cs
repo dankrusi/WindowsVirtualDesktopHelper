294c9f62c330a5be14500cc60d9a9608e7e0001f
Dan Krusi <dan@nerves.ch>
2022-12-20 19:41:15 +0100

    add util classes

---
+++ Source/Util/Logging.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsVirtualDesktopHelper.Util {
+    public class Logging {
+
+        private static List<string> _log = new List<string>();
+
+        public static void WriteLine(string line) {
+            Console.WriteLine(line);
+            _log.Add(line);
+            //TODO: trim front if longer than x lines?
+        }
+
+        public static List<string> GetLogHistory() {
+            return _log;
+        }
+
+    }
+}
---
+++ Source/Util/OS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsVirtualDesktopHelper.Util {
+    public class OS {
+        public static bool IsWindows11() {
+            return GetWindowsBuildVersion() >= 22000;
+        }
+
+        public static int GetWindowsBuildVersion() {
+            // via https://stackoverflow.com/questions/69038560/detect-windows-11-with-net-framework-or-windows-api
+            var reg = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+            var currentBuildStr = (string)reg.GetValue("CurrentBuild");
+            var currentBuild = int.Parse(currentBuildStr);
+            return currentBuild;
+        }
+
+        public static string GetWindowsProductName() {
+            var reg = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+            var retStr = (string)reg.GetValue("ProductName");
+            return retStr;
+        }
+
+        public static string GetWindowsDisplayVersion() {
+            var reg = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+            var retStr = (string)reg.GetValue("DisplayVersion");
+            return retStr;
+        }
+
+        public static int GetWindowsReleaseId() {
+            var reg = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+            var retStr = (string)reg.GetValue("ReleaseId");
+            var retInt = int.Parse(retStr);
+            return retInt;
+        }
+    }
+}
